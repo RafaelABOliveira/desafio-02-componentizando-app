0ba149b4237ca346949f1c46be08242e3dc1d80b
Rafael Antonio de Brito Oliveira <rafael.oliveira@sysmanager.com.br>
2022-05-12 13:55:01 -0300

    Componentização e criação de interface

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+import "../styles/sidebar.scss";
+import { GenreResponseProps } from "../interfaces/GenreResponseProps";
+
+interface SidebarProps {
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number
+}
+
+export function SideBar({ selectedGenreId, handleClickButton }: SidebarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
